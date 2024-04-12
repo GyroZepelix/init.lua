0f4ee7b176edce888f3c8d9181507680599ef15d
GyroZepelix <gamingskullgamer@gmail.com>
2024-04-12 23:20:32 +0200

    treesitter fixed, added harpoon

--- lua/gyro/packer.lua
+++ lua/gyro/packer.lua
@@ -11,4 +11,20 @@ return require('packer').startup(function(use)
 	use { "ellisonleao/gruvbox.nvim" }
 
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+	use('theprimeagen/harpoon')
+
+	use {
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end
+	}
+
 end)
