be34fceaf43919ebc5b2fb7eec1ff0c9ba672ded
GyroZepelix <gamingskullgamer@gmail.com>
2024-04-12 23:41:09 +0200

    added fugitive and undotree

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,6 +4,7 @@ local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
+vim.keymap.set("n", "<Char-0x87>", function() print("test") end)
 vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-ć>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-č>", function() ui.nav_file(3) end)
