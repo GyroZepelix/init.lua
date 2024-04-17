6dab03657adbdd1d51457de82b553edf9f904df0
GyroZepelix <gamingskullgamer@gmail.com>
2024-04-17 22:26:54 +0200

    removed the not working keybind

--- after/plugin/git.lua
+++ after/plugin/git.lua
@@ -1,13 +1,13 @@
-require('gitsigns').setup{
-    on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
-
-        local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-        end
-
-        map('n', '<leader>hd', gitsigns.toggle_deleted)
-    end
-}
+-- require('gitsigns').setup{
+--     on_attach = function(bufnr)
+--         local gitsigns = require('gitsigns')
+--
+--         local function map(mode, l, r, opts)
+--             opts = opts or {}
+--             opts.buffer = bufnr
+--             vim.keymap.set(mode, l, r, opts)
+--         end
+--
+--         map('n', '<leader>hd', gitsigns.toggle_deleted)
+--     end
+-- }
