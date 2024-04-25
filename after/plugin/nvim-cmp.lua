09b371516c7c8f82027117b63df1577e6738445d
GyroZepelix <gamingskullgamer@gmail.com>
2024-04-25 21:36:23 +0200

    changed cmp complete from <C-Space> to <Tab>

--- after/plugin/nvim-cmp.lua
+++ after/plugin/nvim-cmp.lua
@@ -20,7 +20,7 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         -- ['<CR>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<C-Space>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
