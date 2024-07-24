5ef7a17dc56ae70b2216311d30a3fddecc02a92e
GyroZepelix <gamingskullgamer@gmail.com>
2024-07-24 22:12:17 +0200

    changed colorscheme to dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
     {
-        "rose-pine/neovim",
-        name="rose-pine",
-        priority=1000,
-        lazy=false,
-        config = function()
-            vim.cmd("colorscheme rose-pine")
+        'Mofiqul/dracula.nvim',
+        config = function ()
+            vim.cmd('colorscheme dracula')
         end
     }
+    -- {
+    --     "rose-pine/neovim",
+    --     name="rose-pine",
+    --     priority=1000,
+    --     lazy=false,
+    --     config = function()
+    --         vim.cmd("colorscheme rose-pine")
+    --     end
+    -- }
 }
