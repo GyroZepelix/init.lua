c25dc5373b6660f64f5a38793bb6933729935935
GyroZepelix <gamingskullgamer@gmail.com>
2024-10-31 15:00:41 +0100

    Added nvim surround autopairs

--- lua/plugins/nvimsurround-autopairs.lua
+++ lua/plugins/nvimsurround-autopairs.lua
@@ -4,5 +4,10 @@ return {
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
         event = "VeryLazy",
         opts = {}
+    },
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {}
     }
 }
