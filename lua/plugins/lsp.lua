1742eaf08ac180571a8927d751e93852e2559515
GyroZepelix <gamingskullgamer@gmail.com>
2024-10-31 15:00:56 +0100

    Added lsp autoformat

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -128,6 +128,21 @@ return {
                 end
             })
 
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(event)
+                    local id = vim.tbl_get(event, 'data', 'client_id')
+                    local client = id and vim.lsp.get_client_by_id(id)
+                    if client == nil then
+                        return
+                    end
+
+                    -- make sure there is at least one client with formatting capabilities
+                    if client.supports_method('textDocument/formatting') then
+                        require('lsp-zero').buffer_autoformat()
+                    end
+                end
+            })
+
             require('mason-lspconfig').setup({
                 ensure_installed = {},
                 handlers = {
