f7b41be606a7c43bd90ecb03a33b45a48ca3eab8
GyroZepelix <gamingskullgamer@gmail.com>
2024-10-31 14:33:00 +0100

    Fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,15 +2,17 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
-        opts = {
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "typescript", "markdown", "markdown_inline" },
-            sync_install = false,
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        },
+        config = function ()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "typescript", "markdown", "markdown_inline" },
+                sync_install = false,
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            })
+        end
     }
 }
 
