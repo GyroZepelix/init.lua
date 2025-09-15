e10eede12528210f0fb0ceed1bce43a3db353d9d
GyroZepelix <gamingskullgamer@gmail.com>
2025-09-15 12:22:36 +0200

    feat: added more LazyVim-extras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,19 @@
 
 vim.g.snacks_animate = false
 vim.opt.clipboard = ""
+vim.o.winborder = "rounded"
+vim.lsp.inlay_hint.enable(false)
+--
+-- vim.diagnostic.config({
+--   virtual_text = false, -- Optional: disable inline text to reduce clutter
+--   signs = true, -- Keep signs in the sign column
+--   underline = true, -- Underline problematic code
+--   float = {
+--     focusable = false,
+--     style = "minimal",
+--     border = "rounded",
+--     source = "if_many", -- Show the diagnostic source (e.g., gopls)
+--     header = "",
+--     prefix = "",
+--   },
+-- })
