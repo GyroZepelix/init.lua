cc5f08629453f08861b5a84a6e1bac0a3b34f9d6
GyroZepelix <gamingskullgamer@gmail.com>
2025-09-17 13:22:05 +0200

    feat: changed blinkcmp to use super-tab preset

---
+++ lua/plugins/blinkcmp.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        -- Insert completion item on selection, don't select by default
+        list = {
+          selection = {
+            auto_insert = false,
+          },
+        },
+      },
+      keymap = {
+        preset = "super-tab",
+      },
+    },
+  },
+}
