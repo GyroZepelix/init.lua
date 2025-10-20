557eba081cfd18a6bc3f66207656f121918fabf9
GyroZepelix <gamingskullgamer@gmail.com>
2025-10-20 10:04:08 +0200

    feat: added HighlightNumbers command, changed default colorscheme to catppuccin-mocha

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,17 +6,7 @@ vim.g.snacks_animate = false
 vim.opt.clipboard = ""
 vim.o.winborder = "rounded"
 vim.lsp.inlay_hint.enable(false)
---
--- vim.diagnostic.config({
---   virtual_text = false, -- Optional: disable inline text to reduce clutter
---   signs = true, -- Keep signs in the sign column
---   underline = true, -- Underline problematic code
---   float = {
---     focusable = false,
---     style = "minimal",
---     border = "rounded",
---     source = "if_many", -- Show the diagnostic source (e.g., gopls)
---     header = "",
---     prefix = "",
---   },
--- })
+
+vim.api.nvim_create_user_command("HighlightNumbers", function()
+  vim.cmd("highlight LineNr guifg=#59637a")
+end, { nargs = 0 })
