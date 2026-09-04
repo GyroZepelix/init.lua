8328e9501db388814d2c2d097107c2b083bcfe00
GyroZepelix <gamingskullgamer@gmail.com>
2026-09-04 21:58:09 +0200

    feat: added special icons to spec and wiki

---
+++ lua/plugins/mini-icons.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-mini/mini.icons",
+    opts = {
+      directory = {
+        wiki = { glyph = "󰂺", hl = "MiniIconsBlue" },
+        spec = { glyph = "󰈙", hl = "MiniIconsGreen" },
+      },
+    },
+  },
+}
